e414a0021acb7e2e2aaf8c85c71dca7eeb2c6227
Desmond Mullen <dsm@busylittlestudios.com>
2019-03-14 01:02:49 -0400

    workining

--- server.js
+++ server.js
@@ -11,31 +11,6 @@ app.use(express.static(__dirname + '/app/assets'));
 require('./app/routing/apiRoutes')(app);
 require('./app/routing/htmlRoutes')(app);
 
-
-
-
-//take data and make reservation with this constructor
-// const reservationsList = [];
-
-// reservationsList.push({ 'name': 'Desmond', 'email': 'dm@bootcamp.com', 'phone': '919-260-8858', 'uniqueID': '5200', 'feedback': 'reservation', 'position': 1 })
-
-// app.get('/', function (req, res) {
-//     res.sendFile(path.join(__dirname, 'home.html'));
-// });
-
-// app.post('/', function (req, res) {
-//     var reqbody = req.body;
-//     reservation = new Reservation(reqbody.name, reqbody.email, reqbody.phone, reqbody.uniqueID);
-//     if (reservationsList.length < 5) {
-//         reservationsList.push(reservation);
-//         console.log(reservationsList);
-//     } else {
-//         waitingList.push(reservation);
-//         console.log(waitingList);
-//     }
-//     res.json(reservation);
-// });
-
 app.listen(PORT, function () {
     console.log('App listening on PORT ' + PORT);
 });
