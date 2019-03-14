d005850ae94b8ad827512cd2dd39bc1221e5d3f4
Desmond Mullen <dsm@busylittlestudios.com>
2019-03-14 09:24:51 -0400

    deploying

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 var app = express();
-var PORT = 3005;
+var PORT = process.env.PORT || 3005;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
